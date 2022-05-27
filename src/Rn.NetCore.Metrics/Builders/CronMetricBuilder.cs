5131a84fe15e1c3d2bdee3bdaec4ef98a793de9c
Richard Niemand <niemand.richard@gmail.com>
2022-05-27 15:27:32 -0600

    Moving interfaces

--- src/Rn.NetCore.Metrics/Builders/CronMetricBuilder.cs
+++ src/Rn.NetCore.Metrics/Builders/CronMetricBuilder.cs
@@ -6,6 +6,41 @@ using Rn.NetCore.Metrics.Models;
 namespace Rn.NetCore.Metrics.Builders;
 
 // DOCS: docs\builders\CronMetricBuilder.md
+public interface ICronMetricBuilder : IMetricBuilder
+{
+  ICronMetricBuilder ForCronJob(string cronClass, string cronMethod);
+  ICronMetricBuilder WithCategory(string category, string subCategory, bool skipToLower = true);
+  ICronMetricBuilder MarkFailed();
+  ICronMetricBuilder WithException(Exception ex);
+  ICronMetricBuilder WithQueryCount(int queryCount);
+  ICronMetricBuilder IncrementQueryCount(int amount = 1);
+  ICronMetricBuilder WithResultsCount(int resultsCount);
+  ICronMetricBuilder IncrementResultsCount(int amount = 1);
+  ICronMetricBuilder CountResult(object result = null);
+  ICronMetricBuilder WithUserId(int userId);
+  ICronMetricBuilder WithSuccess(bool success);
+  ICronMetricBuilder WithCustomTag1(object value, bool skipToLower = false);
+  ICronMetricBuilder WithCustomTag2(object value, bool skipToLower = false);
+  ICronMetricBuilder WithCustomTag3(object value, bool skipToLower = false);
+  ICronMetricBuilder WithCustomTag4(object value, bool skipToLower = false);
+  ICronMetricBuilder WithCustomInt1(int value);
+  ICronMetricBuilder IncrementCustomInt1(int amount = 1);
+  ICronMetricBuilder WithCustomInt2(int value);
+  ICronMetricBuilder IncrementCustomInt2(int amount = 1);
+  ICronMetricBuilder WithCustomInt3(int value);
+  ICronMetricBuilder IncrementCustomInt3(int amount = 1);
+  ICronMetricBuilder WithCustomLong1(long value);
+  ICronMetricBuilder IncrementCustomLong1(long amount = 0);
+  ICronMetricBuilder WithCustomLong2(long value);
+  ICronMetricBuilder IncrementCustomLong2(long amount = 0);
+  ICronMetricBuilder WithCustomLong3(long value);
+  ICronMetricBuilder IncrementCustomLong3(long amount = 0);
+  IMetricTimingToken WithTiming();
+  IMetricTimingToken WithCustomTiming1();
+  IMetricTimingToken WithCustomTiming2();
+  IMetricTimingToken WithCustomTiming3();
+}
+
 public class CronMetricBuilder : MetricBuilderBase, ICronMetricBuilder
 {
   public bool IsNullMetricBuilder { get; }
--- src/Rn.NetCore.Metrics/Builders/ServiceMetricBuilder.cs
+++ src/Rn.NetCore.Metrics/Builders/ServiceMetricBuilder.cs
@@ -6,6 +6,46 @@ using Rn.NetCore.Metrics.Models;
 namespace Rn.NetCore.Metrics.Builders;
 
 // DOCS: docs\builders\ServiceMetricBuilder.md
+public interface IServiceMetricBuilder : IMetricBuilder
+{
+  IServiceMetricBuilder ForService(string serviceName, string methodName, bool skipToLower = true);
+  IServiceMetricBuilder WithCategory(string category, string subCategory, bool skipToLower = true);
+  IServiceMetricBuilder WithQueryCount(int queryCount);
+  IServiceMetricBuilder IncrementQueryCount(int amount = 1);
+  IServiceMetricBuilder WithResultsCount(int resultsCount);
+  IServiceMetricBuilder IncrementResultsCount(int amount = 1);
+  IServiceMetricBuilder CountResult(object result = null);
+  IServiceMetricBuilder WithException(Exception ex);
+  IServiceMetricBuilder MarkFailed(int resultsCount = -1);
+  IServiceMetricBuilder MarkSuccess(int resultsCount = -1);
+  IServiceMetricBuilder WithUserId(int userId);
+  IServiceMetricBuilder WithSuccess(bool success);
+  IServiceMetricBuilder WithCustomInt1(int value);
+  IServiceMetricBuilder IncrementCustomInt1(int amount = 1);
+  IServiceMetricBuilder WithCustomInt2(int value);
+  IServiceMetricBuilder IncrementCustomInt2(int amount = 1);
+  IServiceMetricBuilder WithCustomInt3(int value);
+  IServiceMetricBuilder IncrementCustomInt3(int amount = 1);
+  IServiceMetricBuilder WithCustomInt4(int value);
+  IServiceMetricBuilder IncrementCustomInt4(int amount = 1);
+  IServiceMetricBuilder WithCustomInt5(int value);
+  IServiceMetricBuilder IncrementCustomInt5(int amount = 1);
+  IServiceMetricBuilder WithCustomLong1(long value);
+  IServiceMetricBuilder IncrementCustomLong1(long amount = 1);
+  IServiceMetricBuilder WithCustomLong2(long value);
+  IServiceMetricBuilder IncrementCustomLong2(long amount = 1);
+  IServiceMetricBuilder WithCustomLong3(long value);
+  IServiceMetricBuilder IncrementCustomLong3(long amount = 1);
+  IServiceMetricBuilder WithCustomTag1(object value, bool skipToLower = false);
+  IServiceMetricBuilder WithCustomTag2(object value, bool skipToLower = false);
+  IServiceMetricBuilder WithCustomTag3(object value, bool skipToLower = false);
+  IServiceMetricBuilder WithCustomTag4(object value, bool skipToLower = false);
+  IMetricTimingToken WithTiming();
+  IMetricTimingToken WithCustomTiming1();
+  IMetricTimingToken WithCustomTiming2();
+  IMetricTimingToken WithCustomTiming3();
+}
+
 public class ServiceMetricBuilder : MetricBuilderBase, IServiceMetricBuilder
 {
   public bool IsNullMetricBuilder { get; }
