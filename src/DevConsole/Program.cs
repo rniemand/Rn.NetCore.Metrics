768fe2962c1b4f049cf98d25fdf4ed7caae4cefb
Richard Niemand <niemand.richard@gmail.com>
2022-05-27 15:42:48 -0600

    Breaking out DI container

--- src/DevConsole/Program.cs
+++ src/DevConsole/Program.cs
@@ -1,48 +1,20 @@
 using System;
-using System.IO;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using NLog.Extensions.Logging;
 using Rn.NetCore.Metrics;
-using Rn.NetCore.Metrics.Extensions;
 
 namespace DevConsole;
 
 internal class Program
 {
-  private static IServiceProvider _services;
+  private static readonly IServiceProvider Services = DIContainer.Get();
 
   static void Main(string[] args)
   {
-    ConfigureDI();
+    var metricsService = Services.GetRequiredService<IMetricService>();
 
-    var metricsService = _services.GetRequiredService<IMetricService>();
+
 
     Console.WriteLine();
     Console.WriteLine();
-  }
-
-  private static void ConfigureDI()
-  {
-    var services = new ServiceCollection();
-
-    var config = new ConfigurationBuilder()
-      .SetBasePath(Directory.GetCurrentDirectory())
-      .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-      .Build();
-
-    services
-      .AddSingleton<IConfiguration>(config)
-      .AddRnMetricsBase()
-
-      .AddLogging(loggingBuilder =>
-      {
-        loggingBuilder.ClearProviders();
-        loggingBuilder.SetMinimumLevel(LogLevel.Trace);
-        loggingBuilder.AddNLog(config);
-      });
-
-    _services = services.BuildServiceProvider();
   }
 }
